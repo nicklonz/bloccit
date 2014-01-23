56fdcdf88da5d8d7387a4d1110f751221d6b7730
Nick L <nicklonz@gmail.com>
2014-01-23 15:08:34 -0500

    Implemented Favoriting

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,15 +39,16 @@ class User < ActiveRecord::Base
     role.nil? ? false : ROLES.index(base_role.to_s) <= ROLES.index(role)
   end
 
+  def favorited(post)
+    self.favorites.where(post_id: post.id).first
+  end
+
   private
 
   def set_member
     self.role = 'member'
   end
   
-  def favorited(post)
-    self.favorites.where(post_id: post.id).first
-  end
-
   private
+
 end
