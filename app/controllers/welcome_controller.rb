906c74932281734cdea38a7a6ce592510e5767cf
Ben Neely <benjaminneely@u.boisestate.edu>
2014-03-19 13:59:41 -0600

    Word cloud

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,23 @@
 class WelcomeController < ApplicationController
   def index
     posts = Post.all
-    word_ary = []
+    word_array = []
+    word_hash = {}
     posts.each do |post|
       post.body.split.each do |word|
-        if word_array.include?
-          word_has
+        if word_array.include?(word)
+          word_hash[word] += 1
+        else
+          word_hash[word] = 1
+        end
       end
     end
+    json_words = []
+
+    word_hash.each do |key, value|
+      json_words << {text: key, weight: value}
+    end
+    @json_words = json_words.to_json
   end
 
   def about
