4693985a23f361b90979787f071849b231312153
Nick L <nicklonz@gmail.com>
2014-03-19 20:14:00 -0400

    Added JQCloud Functionality

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,17 +1,18 @@
 class WelcomeController < ApplicationController
   def index
     posts = Post.all
-    word_array = []
     word_hash = {}
+    
     posts.each do |post|
       post.body.split.each do |word|
-        if word_array.include?(word)
-          word_hash[word] += 1
-        else
+        if word_hash[word].nil?
           word_hash[word] = 1
+        else
+          word_hash[word] += 1
         end
       end
     end
+
     json_words = []
 
     word_hash.each do |key, value|
